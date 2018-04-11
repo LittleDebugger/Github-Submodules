b11bdcce76b0095ddfceb775cc024b7f020cfc3e
Hedley Gisbourne <hedley.gisbourne@icloud.com>
2018-04-11 22:30:53 +0200

    Fixed up Github-Submodules files to use the modues correctly

--- Scripts/demo.js
+++ Scripts/demo.js
@@ -1,17 +1,14 @@
+var canvas = document.getElementById('canvas');
 
-    var canvas = document.getElementById('canvas');
- 
-    var sineWaveGeneratorInstance = sineWaveGenerator(waveOffset, canvas.width);
-    var 
+var sineWaveGeneratorInstance = littleDebugger.audioProcessing.waveGenerators.sineWave(0, canvas.width);
+var simpleVisualiser = littleDebugger.audioProcessing.ui.visualisers.simpleVisualiser(canvas, true, 'red');
 
-    // Reference the sineWave generator.
-    var sineWaveGenerator = littleDebugger.daw.dsp.generator.sineWave;
+var signal = [canvas.width];
 
-    var signal = [canvas.width];
+for (var i = 0; i < canvas.width + 1; i++) {
+    // get sine wave samples at 2 cycles per sample rate.
+    var y = sineWaveGeneratorInstance.getSample(2);
+    signal[i] = y;
+}
 
-    for (var i = 0; i < canvas.width + 1; i++) {
-        // get sine wave samples at 2 cycles per sample rate.
-        var y = sineWaveGeneratorInstance.getSample(2);
-        signal[i] = y;
-    }    
-};
+simpleVisualiser.drawWave(signal);
